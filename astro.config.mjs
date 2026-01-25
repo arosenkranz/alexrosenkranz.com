33b14c8c85d76acb75b0deba02d22c8201ff10ac
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2026-01-25 14:35:44 -0500

    fixes deploy (hopefully)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,20 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alexrosenkranz.com',
   output: 'static',
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
       exclude: ['@astrojs/check'],
     },
   },
+
+  adapter: cloudflare(),
 });
